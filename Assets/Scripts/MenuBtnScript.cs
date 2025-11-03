ab2ea60158327e36df3297a0a37a5ed3d5167567
Busai Dániel <busai.dani@gmail.com>
2025-11-03 11:01:09 +0100

    Made Game Settings Scene
    Implemented hud, no logic yet

--- Assets/Scripts/MenuBtnScript.cs
+++ Assets/Scripts/MenuBtnScript.cs
@@ -5,13 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MGButtonScript : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField]
-    public string TextToDisplay = "default";
-
     void Start()
     {
-        Text textChild = GetComponentInChildren<Text>();
-        textChild.text = TextToDisplay; 
     }
     public void OnPointerClick(PointerEventData eventData)
     {
