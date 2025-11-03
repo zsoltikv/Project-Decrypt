5fcab0ae7549e89f6fd5335ae86b06e38a19859f
Busai Dániel <busai.dani@gmail.com>
2025-11-03 10:17:08 +0100

    created Menu Scene v1
    Created Background for the Menu Scene, Made prefabs for buttons and created a Start and a Credits Button, implemented the scene change to the Start Button

---
+++ Assets/Scripts/MenuBtnScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MGButtonScript : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    public string TextToDisplay = "default";
+
+    void Start()
+    {
+        Text textChild = GetComponentInChildren<Text>();
+        textChild.text = TextToDisplay; 
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (gameObject.name == "GameBtn")
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
