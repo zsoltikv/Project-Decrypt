58192dc9bf4193781dadd00aaf9786148fc47329
zsoltikaa <padarzsolti@gmail.com>
2025-11-06 12:40:56 +0100

    added a new minigame, needs some further enhancements.

---
+++ Assets/Scripts/ConnectorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Connector : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public int connectorID;        // 1, 2, 3
+    public bool isLeftSide;        // bal oldali → true | jobb oldali → false
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (isLeftSide)
+            CableManager.Instance.BeginDrag(this);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        CableManager.Instance.EndDrag(this);
+    }
+}
+
