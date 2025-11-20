60636c6d98ad1bffb15e9e138ff7fb728ac771cf
GG <71087461+immortalized@users.noreply.github.com>
2025-11-20 08:29:41 +0100

    fixed sequencehack outline width becoming thinner when clicked, removed unused passwordSuccess variable

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -25,7 +25,6 @@ public class GameSettingsManager : MonoBehaviour
     public int passLength = 12;
     public string password;
 
-    public bool passwordSuccess = false;
     public bool videoWatched = false;
 
     public int errorCount;
@@ -71,6 +70,7 @@ public class GameSettingsManager : MonoBehaviour
     {
         isSet = false;
         shouldGenAppList = true;
+        videoWatched = false;
         completedApps.Clear();
         appList.Clear();
         currentDifficulty = Difficulty.Normal;
